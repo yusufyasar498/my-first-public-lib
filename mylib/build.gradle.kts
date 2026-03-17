fd1756f91e666673e1453a877bc828e2c2835182
Yusuf Yaşar <yusuf@codify.istanbul>
2026-03-17 11:58:59 +0300

    version 1.0.1

--- mylib/build.gradle.kts
+++ mylib/build.gradle.kts
@@ -44,7 +44,7 @@ afterEvaluate {
 
                 groupId = "com.github.yusufyasar498"
                 artifactId = "my-first-public-lib"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
