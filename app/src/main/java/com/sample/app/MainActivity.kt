6dc43450545afd33bd08ff3b3a102831e91692e1
Yusuf Yaşar <yusuf@codify.istanbul>
2026-03-19 07:56:40 +0300

    test için app modüle eklendi.

---
+++ app/src/main/java/com/sample/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sample.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sample.mylib.LibMainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val fragment = LibMainFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view_tag, fragment)
+            .commit()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyJitpackLib"
 include(":mylib")
+include(":app")
